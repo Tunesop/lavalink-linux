78b31318d4cee7903c1c9aaa4f46af7d51de6ae1
Tunesop <86544219+Tunesop@users.noreply.github.com>
2022-02-01 18:33:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 mkdir LavaLink
+cd LavaLink
 sudo apt install git
 sudo apt install openjdk-13-jre-headless
 wget https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev
