5d236d8a3ea678560871e20bac10ede67013493d
Tunesop <86544219+Tunesop@users.noreply.github.com>
2022-02-01 18:37:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@ cd LavaLink
 sudo apt install git
 sudo apt install openjdk-13-jre-headless
 wget https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev
-git clone https://github.com/Tunesop/lavalink.git .
 java -jar lavalink.jar
